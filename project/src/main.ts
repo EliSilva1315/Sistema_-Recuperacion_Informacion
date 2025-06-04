cae1aafc4989a80de3a5447b87b4ceea2c21a6eb
Elias Bolaños <144160367+ELiasLBT1@users.noreply.github.com>
2025-06-04 16:59:55 -0500

    back

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter([])
+    provideRouter([]),
+    provideHttpClient()
   ]
 });
